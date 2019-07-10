c3acfa4ba2d9d6e23f4bc4d7fa74e95403df7163
dollarstir <kpin463@gmail.com>
2019-07-10 12:13:23 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,104 @@
+print("hello" == "hi")
+#week on LAb Section Tuesday
+print("hello")
+print ('hello')
+#print(hello)
+
+#below are some maths
+print(4 + 5)
+print(8 * 3)
+print(4 / 3)
+print(4.0 / 3)
+print(3 + 5 * 9)
+
+#adding strings
+
+print("abc" + "def")
+print("hello" + " " + "world")
+print("123" + "5")
+
+# Dealing with Variables
+
+fname= "Frederick"
+lname= "Ennin"
+statement =" is a good Student"
+print ( fname + lname + statement)
+
+#my own Application
+firstnames = ["Marian","Miteyan","Dollarsoft","Kent"]
+lastnames= ["Labi","London","Corporation","Baido"]
+print( firstnames[0] + " " + lastnames [0] + statement)
+print( firstnames[1] + " " + lastnames [1] + statement)
+print( firstnames[2] + " " + lastnames [2] + statement)
+print( firstnames[3] + " " + lastnames [3] + statement)
+#end of first App
+
+# User iputs
+name = input("What is your name?")
+print (type(name))
+age = int (input("how old are you?"))
+print(type(age))
+
+print ( name + statement + " and is " + str  (age) + " years old")
+
+#small exercise
+
+import math
+
+
+def deg_to_rad(degree):
+    return degree * (math.pi/180)
+print(deg_to_rad(23))
+
+own= input("type degree ")
+
+print (deg_to_rad(float(own)))
+
+
+#converting from radian to Degre
+def rad_to_degree(radian):
+    return (180 * radian)/math.pi
+
+ran= input ("Input you radian")
+
+print (rad_to_degree(float(ran)))
+
+
+
+# handliing list
+list=["Maxwell","kinet"]
+
+list.append ("kofi")
+print (list)
+list.append("Danso")
+def ll():
+    print (list)
+    
+ll()
+
+list.append (name)
+ll()
+
+
+# dealing  with Whuile Loop
+
+wl=1
+
+while wl<= 6 :
+    
+    print (wl)
+    wl +=1
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,21 @@
+wl=1
+
+while wl< 6 :
+   
+    print (wl)
+    if wl==5:
+        print("5 is enough \n")
+        break
+    wl +=1
+    
+#for loops
+for i in range(1,11):
+    print (i)
+    print("\n")
+
+names= ["kofi","Ama","Afua"]
+
+for dollar in names :
+    print (dollar + " is a good student")
+   
+    
